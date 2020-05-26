5a636eaed201b6d655894cd3b5f624524cbcefe1
Carl Michael Skog <cm@xn--ger-pla.com>
2020-05-26 16:37:39 +0200

    Optional feature package deemed invalid in some cases
    When enumerating optional feature packages, the 'cargo.jq' jq script
    added the dependency objects to each other, to get the "sum" of the objects(
    i.e. all dependencies).
    
    However, if an optional package was in a category(normal, build or dev dependency),
    and it, in addition, was required in "later" category, then the earlier optional
    package was "masked" out(i.e. it was not included among valid optional
    packages).
    
    E.g. the optional package "serde_derive"(applied when setting feature
    "derive" in package "serde"). See example expression in file
    "examples/masked-optional-feature/default.nix".
    When including "serde_derive" in "features", one gets the message
    '"serde_derive" is not a valid feature for package "serde"'.
    
    Fix by filtering on "optional" before adding objects.

---
+++ examples/masked-optional-feature/default.nix
@@ -0,0 +1,162 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.callPackage ({ callPackage, rustChannelOf }:
+    let
+      rustChannel = rustChannelOf {
+        date = "2020-04-22";
+        channel = "nightly";
+      };
+      mkRustCrate = callPackage ../../mkRustCrate/lib/mkRustCrate {
+        inherit (rustChannel) cargo rust;
+      };
+      fetchFromCratesIo = callPackage ../../mkRustCrate/lib/fetchFromCratesIo { };
+    in
+    rec {
+# Package with pkgid: "proc-macro2 1.0.17 (registry+https://github.com/rust-lang/crates.io-index)"
+# A dependency applicable for the following cfgs:
+# "required"
+proc-macro2-1-0-17--registry-https---github-com-rust-lang-crates-io-index- = mkRustCrate rec {
+    name = "proc-macro2";
+    version = "1.0.17";
+    src = fetchFromCratesIo {
+      inherit name version;
+      sha256 = "1md8b4f88x481w8yngdvd34jc2g7270agknd4zdicjs3hq07afnv";
+    };
+    dependencies = [
+unicode-xid-0-2-0--registry-https---github-com-rust-lang-crates-io-index-
+    ];
+    buildDependencies = [
+
+    ];
+    devDependencies = [
+
+    ];
+    features = [
+"default"
+"proc-macro"
+    ];
+  };
+# Package with pkgid: "quote 1.0.6 (registry+https://github.com/rust-lang/crates.io-index)"
+# A dependency applicable for the following cfgs:
+# "required"
+quote-1-0-6--registry-https---github-com-rust-lang-crates-io-index- = mkRustCrate rec {
+    name = "quote";
+    version = "1.0.6";
+    src = fetchFromCratesIo {
+      inherit name version;
+      sha256 = "00d428fwiq1mify90g0a2wpqmnw002qdgm61jcs4fwk2lfmphd05";
+    };
+    dependencies = [
+proc-macro2-1-0-17--registry-https---github-com-rust-lang-crates-io-index-
+    ];
+    buildDependencies = [
+
+    ];
+    devDependencies = [
+
+    ];
+    features = [
+"default"
+"proc-macro"
+    ];
+  };
+# Package with pkgid: "serde 1.0.110 (path+file:///Dev/alacritty/serde)"
+# Root package
+serde-1-0-110--path-file----Dev-alacritty-serde- = mkRustCrate rec {
+    name = "serde";
+    version = "1.0.110";
+    src = /Dev/alacritty/serde;
+    dependencies = [
+serde-derive-1-0-110--registry-https---github-com-rust-lang-crates-io-index-
+    ];
+    buildDependencies = [
+
+    ];
+    devDependencies = [
+serde-derive-1-0-110--registry-https---github-com-rust-lang-crates-io-index-
+    ];
+    features = [
+"derive"
+"serde_derive"
+    ];
+  };
+# Package with pkgid: "serde_derive 1.0.110 (registry+https://github.com/rust-lang/crates.io-index)"
+# A dependency applicable for the following cfgs:
+# "required"
+serde-derive-1-0-110--registry-https---github-com-rust-lang-crates-io-index- = mkRustCrate rec {
+    name = "serde_derive";
+    version = "1.0.110";
+    src = fetchFromCratesIo {
+      inherit name version;
+      sha256 = "0n0gryh5nppibvqzdrwrd8gs35mfc65r7k4gzswjplihbh1hqbfn";
+    };
+    dependencies = [
+proc-macro2-1-0-17--registry-https---github-com-rust-lang-crates-io-index-
+quote-1-0-6--registry-https---github-com-rust-lang-crates-io-index-
+syn-1-0-24--registry-https---github-com-rust-lang-crates-io-index-
+    ];
+    buildDependencies = [
+
+    ];
+    devDependencies = [
+
+    ];
+    features = [
+"default"
+    ];
+  };
+# Package with pkgid: "syn 1.0.24 (registry+https://github.com/rust-lang/crates.io-index)"
+# A dependency applicable for the following cfgs:
+# "required"
+syn-1-0-24--registry-https---github-com-rust-lang-crates-io-index- = mkRustCrate rec {
+    name = "syn";
+    version = "1.0.24";
+    src = fetchFromCratesIo {
+      inherit name version;
+      sha256 = "05wl9iix808ajigand9f8lpgpx7dg3xkfcqqwyqic8nsyvikw2j8";
+    };
+    dependencies = [
+proc-macro2-1-0-17--registry-https---github-com-rust-lang-crates-io-index-
+quote-1-0-6--registry-https---github-com-rust-lang-crates-io-index-
+unicode-xid-0-2-0--registry-https---github-com-rust-lang-crates-io-index-
+    ];
+    buildDependencies = [
+
+    ];
+    devDependencies = [
+
+    ];
+    features = [
+"clone-impls"
+"default"
+"derive"
+"parsing"
+"printing"
+"proc-macro"
+"quote"
+"visit"
+    ];
+  };
+# Package with pkgid: "unicode-xid 0.2.0 (registry+https://github.com/rust-lang/crates.io-index)"
+# A dependency applicable for the following cfgs:
+# "required"
+unicode-xid-0-2-0--registry-https---github-com-rust-lang-crates-io-index- = mkRustCrate rec {
+    name = "unicode-xid";
+    version = "0.2.0";
+    src = fetchFromCratesIo {
+      inherit name version;
+      sha256 = "1c85gb3p3qhbjvfyjb31m06la4f024jx319k10ig7n47dz2fk8v7";
+    };
+    dependencies = [
+
+    ];
+    buildDependencies = [
+
+    ];
+    devDependencies = [
+
+    ];
+    features = [
+"default"
+    ];
+  };
+    }) {}
