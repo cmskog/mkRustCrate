891ef43617df787691629e13ffb8335b2a419f84
Carl Michael Skog <cm@xn--ger-pla.com>
2021-05-07 02:35:19 +0200

    Support for cratelocation for fetchFromCratesIo
    Argument cratelocation points to the source from which fetchFromCratesIo
    should load the crate source.
    
    This allows you e.g. to determine checksums based on the local crate
    cache, and then check this against the remote crate registry(after
    removing the crate source paths from the nix store).

--- mkRustCrate/lib/fetchFromCratesIo/default.nix
+++ mkRustCrate/lib/fetchFromCratesIo/default.nix
@@ -1,8 +1,27 @@
-{ name, version, ... } @ args:
+{ name, version, cratelocation ? null, ... } @ args:
 let
-  args' = builtins.removeAttrs args ["name" "version"];
-in
-  builtins.fetchTarball ({
+  args' = builtins.removeAttrs args ["name" "version" "cratelocation"];
+  crateinfo = {
     name = "${name}-${version}.crate";
-    url = "https://crates.io/api/v1/crates/${name}/${version}/download";
-  } // args')
+    } // args';
+in
+    if cratelocation == null ||
+       cratelocation == "remoteregistry"
+    then
+      builtins.fetchTarball ({
+        url = "https://crates.io/api/v1/crates/${name}/${version}/download";
+      } // crateinfo)
+    else if cratelocation == "localcargosrc"
+    then
+      builtins.path ({
+        path = (/. + (builtins.getEnv "HOME")  + "/.cargo/registry/src/github.com-1ecc6299db9ec823/${name}-${version}");
+        filter = (path: type: type != "regular" || baseNameOf path != ".cargo-ok");
+        recursive = true;
+      } // crateinfo)
+    else if cratelocation == "localcargocrate"
+    then
+      builtins.fetchTarball ({
+        url = ("file://" + (builtins.getEnv "HOME")  + "/.cargo/registry/cache/github.com-1ecc6299db9ec823/${name}-${version}.crate");
+      } // crateinfo)
+    else
+      builtins.abort "fetchFromCratesIo(${crateinfo.name}): invalid cratelocation: ${cratelocation}"
